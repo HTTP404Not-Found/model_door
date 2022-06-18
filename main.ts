25da37d4b9455a531916f734f69061f1930fd164
HTTP404 <46433848+HTTP404Not-Found@users.noreply.github.com>
2022-06-18 12:25:10 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("bye")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("ok")
+})
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
 radio.setGroup(1)
 basic.forever(function () {
     huskylens.request()
-    if (huskylens.isLearned(1)) {
-        radio.sendString("Ok")
-        basic.pause(1000)
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        radio.sendString("ok")
     }
 })
